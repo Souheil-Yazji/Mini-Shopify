ca46203320655386df7056a63597eab1ff6b830e
Derek Shao <shao.derek.now@gmail.com>
2021-03-03 17:47:42 -0500

    IntelliJ Project Starter (#8)
    * feat: Project setup
    * fix: PostgreSQL dependencies

---
+++ src/main/java/com/shopify/minishopify/MiniShopifyApplication.java
@@ -0,0 +1,12 @@
+package com.shopify.minishopify;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MiniShopifyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MiniShopifyApplication.class, args);
+    }
+}
