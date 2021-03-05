558394c62915b3a7ab051201fd064ef02c487af3
Derek Shao <shao.derek.now@gmail.com>
2021-03-05 09:07:59 -0500

    feat: add user table

--- src/main/resources/db/migration/V002__Shopify_tables.sql
+++ src/main/resources/db/migration/V002__Shopify_tables.sql
@@ -7,3 +7,9 @@ create table migrations.category (
   id bigint generated always as identity primary key,
   name text not null
 );
+
+create table migrations.user (
+  id bigint generated always as identity primary key,
+  email text not null,
+  name text not null
+);
