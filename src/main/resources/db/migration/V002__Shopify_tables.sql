1faa19967684639973d2f9b88a82ad0925bc937a
Hoang Bui <hoangxuanbui6572@gmail.com>
2021-03-05 09:55:44 -0500

    feat: Added the product and shop_category table

--- src/main/resources/db/migration/V002__Shopify_tables.sql
+++ src/main/resources/db/migration/V002__Shopify_tables.sql
@@ -24,3 +24,18 @@ create table migrations.shop (
     FOREIGN KEY (owner_id)
       REFERENCES migrations.user(id)
 );
+
+create table migrations.product(
+    id bigint generated always as identity primary key,
+    shop_id BIGINT REFERENCES migrations.shop(id),
+    name text not null,
+    price BIGINT not null,
+    quantity BIGINT not null,
+    image bytea not null
+);
+
+create table migrations.shop_category(
+    shop_id BIGINT REFERENCES migrations.shop (id),
+    category_id BIGINT REFERENCES migrations.category (id),
+    CONSTRAINT shop_category_id PRIMARY KEY (shop_id, category_id)
+);
