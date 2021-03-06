6ffcdc8546c4ce101f427a35419ed47731486065
Michael <mfan20107@gmail.com>
2021-03-06 11:36:06 -0500

    wip: restructure client and create spa controller

--- src/main/java/com/shopify/minishopify/SPAController.java
+++ src/main/java/com/shopify/minishopify/SPAController.java
@@ -3,8 +3,12 @@ package com.shopify.minishopify;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
+/**
+ * This is the SPA controller that will return index.html for requests made to /app/**.
+ * All frontend routing should have base path /app.
+ */
 @Controller
-public class HomeController {
+public class SPAController {
 
     @GetMapping("/app/**")
     public String helloWorld() {
