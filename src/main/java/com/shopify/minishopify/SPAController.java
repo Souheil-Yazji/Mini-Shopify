8257775ef2040512c97068453ca3f57641296cc5
Michael <mfan20107@gmail.com>
2021-03-06 11:02:57 -0500

    wip: restructure client and create spa controller

--- src/main/java/com/shopify/minishopify/SPAController.java
+++ src/main/java/com/shopify/minishopify/SPAController.java
@@ -1,15 +1,13 @@
 package com.shopify.minishopify;
 
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
 
-@RestController
+@Controller
 public class HomeController {
 
-
-    @GetMapping("/home")
+    @GetMapping("/app/**")
     public String helloWorld() {
-
-        return "Hello, Mini-Shopify!";
+        return "index";
     }
 }
