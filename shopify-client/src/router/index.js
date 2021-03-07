21ff511f8a8f9a4152880e46b035112531e92029
Michael <mfan20107@gmail.com>
2021-03-07 10:03:41 -0500

    wip: add product details view

--- shopify-client/src/router/index.js
+++ shopify-client/src/router/index.js
@@ -1,29 +1,38 @@
-import Vue from 'vue'
-import VueRouter from 'vue-router'
-import Home from '../views/Home.vue'
+import Vue from "vue";
+import VueRouter from "vue-router";
+import Home from "../views/Home.vue";
 
-Vue.use(VueRouter)
+import ProductDetails from "../views/ProductDetails.vue";
+
+Vue.use(VueRouter);
 
 const routes = [
   {
-    path: '/app',
-    name: 'Home',
-    component: Home
+    path: "/app",
+    name: "Home",
+    component: Home,
   },
   {
-    path: '/app/about',
-    name: 'About',
+    path: "/app/products/:id",
+    name: "ProductDetails",
+    component: ProductDetails,
+    props: true,
+  },
+  {
+    path: "/app/about",
+    name: "About",
     // route level code-splitting
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
-  }
-]
+    component: () =>
+      import(/* webpackChunkName: "about" */ "../views/About.vue"),
+  },
+];
 
 const router = new VueRouter({
-  mode: 'history',
+  mode: "history",
   base: process.env.BASE_URL,
-  routes
-})
+  routes,
+});
 
-export default router
+export default router;
