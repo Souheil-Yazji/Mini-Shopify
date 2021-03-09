93d4b64f5ef081cc41fdf1a67f826431488340e8
Derek Shao <shao.derek.now@gmail.com>
2021-03-09 10:10:30 -0500

    wip: shop details template

--- shopify-client/src/views/ShopDetails.vue
+++ shopify-client/src/views/ShopDetails.vue
@@ -1,44 +1,59 @@
 <template>
     <div>
         <p v-if="error">{{ error }}</p>
-        <div v-if="shop" class="text-center">
-            <b-container id="shop-intro">
-                <b-row>
-                    <b-col sm="6">
-                        <b-row>
-                            <h1>{{ shop.name }}</h1>
-                        </b-row>
-                        <b-row>
-                            <h4>{{ shop.description }}</h4>
-                        </b-row>
-                        <b-row>
-                            <b-button>
-                                Browse Products
-                            </b-button>
-                        </b-row>
-                    </b-col>
-                    <b-col sm="6">
-                        <b-row>
+        <div v-if="shop">
+            <div id="shop-intro-wrapper">
+                <b-container id="shop-intro">
+                    <b-row>
+                        <b-col sm="6">
+                            <b-row>
+                                <h1>{{ shop.name }}</h1>
+                            </b-row>
+                            <b-row>
+                                <h4>{{ shop.description }}</h4>
+                            </b-row>
+                            <b-row>
+                                <b-button v-on:click="scrollToProductCatalog({behavior: 'smooth'})">
+                                    Browse Products
+                                </b-button>
+                            </b-row>
+                        </b-col>
+                        <b-col sm="6">
                             <b-col sm="12">
                                 <b-img v-bind:src="shop.image"></b-img>
                             </b-col>
+                        </b-col>
+                    </b-row>
+                </b-container>
+            </div>
+            <div id="product-catalog-wrapper">
+                <b-container id="productCatalog" ref="productCatalog">
+                    <h2>Product Catalog</h2>
+                    <div v-if="shop.products.length">
+                        <b-row cols="4">
+                            <b-col v-for="product in shop.products" :key="product.name">
+                                <b-card
+                                        v-bind:title="product.name"
+                                        v-bind:img-src="product.image"
+                                        v-bind:img-alt="product.name"
+                                        img-top
+                                        tag="article"
+                                        style="max-width: 20rem;"
+                                        class="mb-2"
+                                >
+                                    <b-card-text>
+                                        {{ product.description }}
+                                    </b-card-text>
+
+                                    <b-button v-bind:href="'/app/products/' + product.id" variant="info">Details</b-button>
+                                    <b-button href="#" variant="primary">Add to Cart</b-button>
+                                </b-card>
+                            </b-col>
                         </b-row>
-                    </b-col>
-                </b-row>
-            </b-container>
-            <b-container id="product-catalog">
-                <b-col v-for="product in shop.products" :key="product.name">
-                    <b-row>
-                        {{ product.name }}
-                    </b-row>
-                    <b-row>
-                        {{ product.description}}
-                    </b-row>
-                    <b-row>
-                        {{ product.price }}
-                    </b-row>
-                </b-col>
-            </b-container>
+                    </div>
+                    <p v-else>No products in shop</p>
+                </b-container>
+            </div>
         </div>
     </div>
 </template>
@@ -49,8 +64,17 @@
         data: function () {
             return {
                 shop: null,
-                error: "",
+                error: ""
             };
+        },
+        methods: {
+            scrollToProductCatalog(options) {
+                const el = this.$refs.productCatalog;
+
+                if (el) {
+                    el.scrollIntoView(options);
+                }
+            }
         },
         props: ["id"],
         created: function () {
@@ -64,10 +88,7 @@
 
                     return response;
                 })
-                .then((data) => {
-                    console.log(data);
-                    vm.shop = data
-                })
+                .then((data) => (vm.shop = data))
                 .catch((error) => {
                     vm.error = error;
                 });
@@ -75,4 +96,14 @@
     };
 </script>
 
-<style scoped></style>
+<style scoped>
+
+    #shop-intro-wrapper {
+        background-color: #fbf7ed;
+    }
+
+    #shop-intro {
+        height: 680px;
+    }
+
+</style>
