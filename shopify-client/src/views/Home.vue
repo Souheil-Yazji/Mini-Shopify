ec068e3cfb8f4f20e958fa33fe3632a4bf9583c7
Souheil <35379392+Souheil-Yazji@users.noreply.github.com>
2021-03-10 20:35:02 -0500

    Feat: added home page (#49)
    Co-authored-by: Michael Fan <mfan20107@gmail.com>

--- shopify-client/src/views/Home.vue
+++ shopify-client/src/views/Home.vue
@@ -1,18 +1,43 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div id="home">
+    <div>
+      <b-jumbotron :fluid="true" :container-fluid="true">
+
+        <template #header>
+          <img alt="MiniSopify logo" src="../assets/MiniShopLogo.png">
+        </template>
+
+        <template #lead>
+          Welcome to the one-stop-shop of many shops!
+        </template>
+
+        <hr class="my-4">
+
+        <p>
+          Looking for a particular shop or product?
+          <b-button variant="outline-dark" :to="'/app/shops/list'">Browse Here</b-button>
+
+        </p>
+        <p>looking to open your own shop?
+          <b-button variant="outline-dark" :to="'/app/shops/create'">Start Here</b-button>
+
+        </p>
+      </b-jumbotron>
+    </div>
+
   </div>
+
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
-
-export default {
-  name: 'Home',
-  components: {
-    HelloWorld
-  }
-}
 </script>
+
+<style>
+
+#home {
+  margin: auto;
+  width: 50%;
+  text-align: center;
+}
+
+</style>
