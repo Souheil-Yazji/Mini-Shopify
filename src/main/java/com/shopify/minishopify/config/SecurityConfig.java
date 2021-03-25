d8d3c64ad679f2a0a6b729f2e52ef59675ad6a0f
Michael Fan <mfan20107@gmail.com>
2021-03-24 21:48:10 -0400

    wip: add csp

---
+++ src/main/java/com/shopify/minishopify/config/SecurityConfig.java
@@ -0,0 +1,13 @@
+package com.shopify.minishopify.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@Configuration
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.headers().xssProtection().and().contentSecurityPolicy("script-src 'self'");
+    }
+}
