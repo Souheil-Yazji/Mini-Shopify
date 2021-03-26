d90a03675c0eb7ff944cb270fe13b5bec8f81155
Souheil-Yazji <35379392+Souheil-Yazji@users.noreply.github.com>
2021-03-25 23:20:02 -0400

    Feat: added auth views

---
+++ shopify-client/src/views/Login.vue
@@ -0,0 +1,32 @@
+<template>
+  <div class="main-container">
+    <form>
+      <div class="box-container">
+        <h2 class="heading">Login</h2>
+        <div class="form-fields">
+          <input id="email" name="email" type="text" placeholder="Email Address">
+        </div>
+        <div class="form-fields">
+          <input id="password" name="password" type="text" placeholder="Password">
+        </div>
+        <div class="form-fields">
+          <button class="signIn" name="commit" type="submit">
+            Sign In
+          </button>
+        </div>
+      </div>
+    </form>
+    <div class="footer">
+      <p>Don't have an account?
+        <router-link to=signup> Create one now</router-link>
+      </p>
+    </div>
+  </div>
+</template>
+
+<script>
+
+</script>
+
+<style scoped>
+</style>
---
+++ shopify-client/src/views/SignUp.vue
@@ -0,0 +1,44 @@
+<template>
+  <div class="main-container">
+    <form>
+      <div class="box-container">
+        <h2 class="heading">
+          Create Your Account
+        </h2>
+        <div class="form-fields">
+          <input id="user_name" name="username" type="text" placeholder="Full name">
+        </div>
+        <div class="form-fields">
+          <input id="email" name="email" type="text" placeholder="Email Address">
+        </div>
+        <div class="form-fields">
+          <input id="password" name="password" type="text" placeholder="Password">
+        </div>
+        <div class="form-fields">
+          <button class="createaccount" name="commit" type="submit">
+            Create your account
+          </button>
+        </div>
+        <div class="login-choice"><span>or sign up with</span></div>
+        <o-auth/>
+        <div>
+          <p class="center">
+            By signing up you agree to the
+            <a href="#">Terms of Service</a>.
+          </p>
+        </div>
+      </div>
+    </form>
+    <div class="footer">
+      <p>Already have an account?
+        <router-link to=Login> Login</router-link>
+      </p>
+    </div>
+  </div>
+</template>
+
+<script>
+</script>
+
+<style scoped>
+</style>
